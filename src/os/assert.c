edde8957d6e4b46b1cf4607095f2e033d3fa06cc
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2021-12-25 14:32:24 -0500

    Reverted rcp.h and added missing defines from 2.0L, fixed assert compilation error

--- src/os/assert.c
+++ src/os/assert.c
@@ -1,5 +1,5 @@
 #include "PR/os_internal.h"
 
 void __assert(const char *exp, const char *filename, int line) {
-    osSyncPrintf("\nASSERTION FAULT: %s, %d: \"%s\"\n", filename, line, arg0);
+    osSyncPrintf("\nASSERTION FAULT: %s, %d: \"%s\"\n", filename, line, exp);
 }
