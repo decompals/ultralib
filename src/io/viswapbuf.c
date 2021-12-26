5d9546581634a6c8ec90ed6eaf91fecb90fd2176
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2021-12-26 12:45:56 -0500

    Matched RSP (os/sp), region (rg), and libc C files

--- src/io/viswapbuf.c
+++ src/io/viswapbuf.c
@@ -8,6 +8,6 @@ void osViSwapBuffer(void* frameBufPtr) {
     u32 saveMask = __osDisableInt();
 
     __osViNext->framep = frameBufPtr;
-    __osViNext->state |= VI_STATE_10;
+    __osViNext->state |= VI_STATE_BUFFER_UPDATED;
     __osRestoreInt(saveMask);
 }
