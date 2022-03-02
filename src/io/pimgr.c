e6800e2fba5f1e2ca0b110dccb79a4cd0955b3e0
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2022-03-02 18:41:21 -0500

    Fixed formatting issues in pidma.c and pimgr.c

--- src/io/pimgr.c
+++ src/io/pimgr.c
@@ -13,41 +13,41 @@ static OSMesgQueue piEventQueue;
 static OSMesg piEventBuf[1];
 
 void osCreatePiManager(OSPri pri, OSMesgQueue *cmdQ, OSMesg *cmdBuf, s32 cmdMsgCnt) {
-	u32 savedMask;
-	OSPri oldPri;
-	OSPri myPri;
+    u32 savedMask;
+    OSPri oldPri;
+    OSPri myPri;
 
-	if (!__osPiDevMgr.active) {
-		osCreateMesgQueue(cmdQ, cmdBuf, cmdMsgCnt);
-		osCreateMesgQueue(&piEventQueue, (OSMesg*)piEventBuf, 1);
-		
-		if (!__osPiAccessQueueEnabled) {
-			__osPiCreateAccessQueue();
-		}
-		
-		osSetEventMesg(OS_EVENT_PI, &piEventQueue, (OSMesg)0x22222222);
-		oldPri = -1;
-		myPri = osGetThreadPri(NULL);
+    if (!__osPiDevMgr.active) {
+        osCreateMesgQueue(cmdQ, cmdBuf, cmdMsgCnt);
+        osCreateMesgQueue(&piEventQueue, (OSMesg*)piEventBuf, 1);
+        
+        if (!__osPiAccessQueueEnabled) {
+            __osPiCreateAccessQueue();
+        }
+        
+        osSetEventMesg(OS_EVENT_PI, &piEventQueue, (OSMesg)0x22222222);
+        oldPri = -1;
+        myPri = osGetThreadPri(NULL);
 
-		if (myPri < pri) {
-			oldPri = myPri;
-			osSetThreadPri(NULL, pri);
-		}
+        if (myPri < pri) {
+            oldPri = myPri;
+            osSetThreadPri(NULL, pri);
+        }
 
-		savedMask = __osDisableInt();
-		__osPiDevMgr.active = 1;
-		__osPiDevMgr.thread = &piThread;
-		__osPiDevMgr.cmdQueue = cmdQ;
-		__osPiDevMgr.evtQueue = &piEventQueue;
-		__osPiDevMgr.acsQueue = &__osPiAccessQueue;
-		__osPiDevMgr.dma = __osPiRawStartDma;
-		__osPiDevMgr.edma = __osEPiRawStartDma;
-		osCreateThread(&piThread, 0, __osDevMgrMain, &__osPiDevMgr, &piThreadStack[OS_PIM_STACKSIZE], pri);
-		osStartThread(&piThread);
-		__osRestoreInt(savedMask);
+        savedMask = __osDisableInt();
+        __osPiDevMgr.active = 1;
+        __osPiDevMgr.thread = &piThread;
+        __osPiDevMgr.cmdQueue = cmdQ;
+        __osPiDevMgr.evtQueue = &piEventQueue;
+        __osPiDevMgr.acsQueue = &__osPiAccessQueue;
+        __osPiDevMgr.dma = __osPiRawStartDma;
+        __osPiDevMgr.edma = __osEPiRawStartDma;
+        osCreateThread(&piThread, 0, __osDevMgrMain, &__osPiDevMgr, &piThreadStack[OS_PIM_STACKSIZE], pri);
+        osStartThread(&piThread);
+        __osRestoreInt(savedMask);
 
-		if (oldPri != -1) {
-			osSetThreadPri(NULL, oldPri);
-		}
-	}
+        if (oldPri != -1) {
+            osSetThreadPri(NULL, oldPri);
+        }
+    }
 }
