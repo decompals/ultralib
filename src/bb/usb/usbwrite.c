b5efda7f48ae118b3d9b504123ce1e779336031b
Tharo <17233964+Thar0@users.noreply.github.com>
2022-09-14 22:17:52 +0100

    [iQue] A few USB files (#29)
    * Some usb files
    
    * Macros
    
    * General align macro for powers of 2

---
+++ src/bb/usb/usbwrite.c
@@ -0,0 +1,44 @@
+#include "PR/os_internal.h"
+#include "PR/rcp.h"
+
+#include "usb.h"
+
+#ident "$Revision: 1.1 $"
+
+s32 osBbUsbDevWrite(OSBbUsbHandle h, u8* buf, s32 len, u64 off) {
+    _usb_ext_handle* uhp = (_usb_ext_handle*)h;
+    __OSBbUsbMesg um;
+    OSMesg msg = (OSMesg)&um;
+    OSMesg rmsg;
+    OSMesgQueue rq;
+    s32 wtotal;
+
+    if (uhp->uh_host_handle == NULL) {
+        return -0xC6;
+    }
+
+    bzero(&um, sizeof(um));
+    um.u.umrw.umrw_handle = h;
+    um.um_rq = &rq;
+
+    osCreateMesgQueue(&rq, &rmsg, 1);
+    osInvalDCache(buf, len);
+
+    wtotal = 0;
+    while (len > 0) {
+        um.u.umrw.umrw_buffer = buf;
+        um.u.umrw.umrw_len = len;
+        um.u.umrw.umrw_offset = off;
+        um.um_type = 5;
+
+        osSendMesg(uhp->uh_mq, msg, OS_MESG_BLOCK);
+        osRecvMesg(&rq, &msg, OS_MESG_BLOCK);
+
+        wtotal = um.um_ret;
+        if (um.um_ret >= 0) {
+            wtotal = len;
+        }
+        len = 0;
+    }
+    return wtotal;
+}
