4e732a9af1bfefa889814ed83604d56b6f9922c9
Tharo <17233964+Thar0@users.noreply.github.com>
2022-09-15 02:07:02 +0100

    Defines for constants and other cleanup (#1)

--- src/monutil.s
+++ src/monutil.s
@@ -4,125 +4,128 @@
 #include "PR/R4300.h"
 #include "PR/ultratypes.h"
 
+#define EXC(code) (EXC_##code >> CAUSE_EXCSHIFT)
+
 .text
 LEAF(__isExpJP)
 .set noreorder
-    j __isExp
+    j       __isExp
      nop
-    sw k0, -0x10(sp)
-    la k0, __isExp
-    jr k0
+    sw      k0, -0x10(sp)
+    la      k0, __isExp
+    jr      k0
      nop
 .set reorder
 END(__isExpJP)
 
 LEAF(__isExp)
 .set noreorder
-    sw k0, -0x10(sp)
-    sw k1, -8(sp)
-    mfc0 k0, C0_CAUSE
+    sw      k0, -0x10(sp)
+    sw      k1, -8(sp)
+    mfc0    k0, C0_CAUSE
     nop
-    andi k0, k0, 0x7C
-    bnez k0, non_stop
+    andi    k0, k0, CAUSE_EXCMASK
+    bnez    k0, non_stop
      nop
-    mfc0 k0, C0_CAUSE
+    mfc0    k0, C0_CAUSE
     nop
-    andi k0, k0, 0x4000
-    beqz k0, throw_os_exception
+    andi    k0, k0, CAUSE_IP7
+    beqz    k0, throw_os_exception
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_stop:
-    sra k0, k0, 2
-    addiu k1,  zero, 9
-    bne k0, k1, non_bp
+    sra     k0, k0, CAUSE_EXCSHIFT
+    li      k1, EXC(BREAK)
+    bne     k0, k1, non_bp
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_bp:
-    addiu k1, zero, 4
-    bne k0, k1, non_adrs_store_exp
+    li      k1, EXC(RADE)
+    bne     k0, k1, non_adrs_store_exp
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_adrs_store_exp:
-    addiu k1, zero, 5
-    bne k0, k1, non_adrs_load_exp
+    li      k1, EXC(WADE)
+    bne     k0, k1, non_adrs_load_exp
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_adrs_load_exp:
-    addiu k1, zero, 6
-    bne k0, k1, non_bus_code_exp
+    li      k1, EXC(IBE)
+    bne     k0, k1, non_bus_code_exp
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_bus_code_exp:
-    addiu k1, zero, 7
-    bne k0, k1, non_bus_data_exp
+    li      k1, EXC(DBE)
+    bne     k0, k1, non_bus_data_exp
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_bus_data_exp:
-    addiu k1, zero, 1
-    bne k0, k1, non_tlb_mod
+    li      k1, EXC(MOD)
+    bne     k0, k1, non_tlb_mod
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_tlb_mod:
-    addiu k1, zero, 2
-    bne k0, k1, non_tlb_load
+    li      k1, EXC(RMISS)
+    bne     k0, k1, non_tlb_load
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_tlb_load:
-    addiu k1, zero, 3
-    bne k0, k1, non_tlb_store
+    li      k1, EXC(WMISS)
+    bne     k0, k1, non_tlb_store
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_tlb_store:
-    addiu k1, zero, 0xA
-    bne k0, k1, non_resv
+    li      k1, EXC(II)
+    bne     k0, k1, non_resv
      nop
-    j go_monitor
+    j       go_monitor
      nop
 
 non_resv:
-    lw k1, -8(sp)
+    lw      k1, -8(sp)
+
 throw_os_exception:
-    j ramOldVector
+    j       ramOldVector
      nop
 
 go_monitor:
-    move k0, sp
-    addiu sp, sp, -0x200
-    sw k0, 0x78(sp)
-    lw k1, -8(k0)
-    sw k1, 0x70(sp)
-    lw k1, -0x10(k0)
-    sw k1, 0x6c(sp)
+    move    k0, sp
+    addiu   sp, sp, -0x200
+    sw      k0, 0x78(sp)
+    lw      k1, -8(k0)
+    sw      k1, 0x70(sp)
+    lw      k1, -0x10(k0)
+    sw      k1, 0x6c(sp)
     nop
 
 w:
-    lui k0, 0xA460
-    lw k0, 0x10(k0)
+    lui     k0, (0xA4600010 >> 16)
+    lw      k0, (0xA4600010 & 0xFFFF)(k0)   /* PI_STATUS_REG */
     nop
-    andi k0, k0, 3
-    bnez k0, w
+    andi    k0, k0, 3           /* (PI_STATUS_DMA_BUSY | PI_STATUS_IO_BUSY) */
+    bnez    k0, w
      nop
-    lui k0, 0xBFF0
-    addiu k0, k0, 0x50
-    jr k0
+
+    la      k0, 0xBFF00050
+    jr      k0
      nop
 .set reorder
 END(__isExp)
@@ -132,11 +135,11 @@ LEAF(MonitorInitBreak)
     nop
     nop
 INIT_BREAK_POSITION:
-    break 4
+    break   4
     nop
     nop
     nop
-    jr ra
+    jr      ra
      nop
 .set reorder
 END(MonitorInitBreak)
