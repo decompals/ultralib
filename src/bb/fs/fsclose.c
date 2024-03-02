c8bc13ac834def95c6b628bdc7d2937ce21e0724
angie <angheloalf95@gmail.com>
2024-03-02 19:36:09 -0300

    fsclose.c

---
+++ src/bb/fs/fsclose.c
@@ -0,0 +1,23 @@
+#include "PR/os_internal.h"
+#include "PR/bbfs.h"
+
+s32 osBbFClose(s32 fd) {
+    BbFat16* fat;
+    s32 rv;
+
+    if (fd >= 0x199U) {
+        return -3;
+    }
+
+    rv = __osBbFsGetAccess();
+    if (rv != 0) {
+        return rv;
+    }
+
+    fat = __osBbFat;
+    if (fat->inode[fd].type == 0) {
+        rv = -3;
+    }
+    __osBbFsRelAccess();
+    return rv;
+}
