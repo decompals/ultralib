16441de4d6cdf055f3acfa305ef53277c166f65f
angie <angheloalf95@gmail.com>
2024-03-03 10:02:08 -0300

    fsrename.c

---
+++ src/bb/fs/fsrename.c
@@ -0,0 +1,63 @@
+#include "PR/os_internal.h"
+#include "PR/bbfs.h"
+
+s32 osBbFRename(const char* old, const char* new) {
+    unsigned char fold[BB_INODE16_NAMELEN];
+    unsigned char fnew[BB_INODE16_NAMELEN];
+    s32 rv;
+
+    s32 inew;
+    s32 iold;
+
+    BbFat16* fat;
+    int i;
+
+    inew = -1;
+    iold = -1;
+
+    __osBbFsFormatName(fold, old);
+    __osBbFsFormatName(fnew, new);
+
+    if ((fold[0] == '\0') || (fnew[0] == '\0')) {
+        return -3;
+    }
+
+    rv = __osBbFsGetAccess();
+    if (rv < 0) {
+        return rv;
+    }
+
+    fat = __osBbFat;
+
+    for (i = 0; i < BB_INODE16_NUM; i++) {
+        if (fat->inode[i].type != 0) {
+            if (bcmp(fnew, fat->inode[i].name, BB_INODE16_NAMELEN * sizeof(unsigned char)) == 0) {
+                inew = i;
+            } else if (bcmp(fold, fat->inode[i].name, BB_INODE16_NAMELEN * sizeof(unsigned char)) == 0) {
+                iold = i;
+            }
+        }
+    }
+
+    rv = -8;
+    if (iold != -1) {
+        if (inew != -1) {
+            u16 b;
+
+            b = fat->inode[inew].block;
+            while (b != 0xFFFF) {
+                u16 temp;
+
+                temp = fat[b >> 0xC].entry[b & 0xFFF];
+                fat[b >> 0xC].entry[b & 0xFFF] = 0;
+                b = temp;
+            }
+            bzero(&fat->inode[inew], 0x14);
+        }
+        bcopy(fnew, fat->inode[iold].name, BB_INODE16_NAMELEN * sizeof(unsigned char));
+        rv = __osBbFsSync(0);
+    }
+
+    __osBbFsRelAccess();
+    return rv;
+}
