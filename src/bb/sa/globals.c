d1b96a4107d06e7042d2a0a1a1ea145ea597bf28
AltoXorg <56553686+Alto1772@users.noreply.github.com>
2024-03-04 22:00:19 +0800

    [iQue] Decompile string functions (#68)

---
+++ src/bb/sa/globals.c
@@ -0,0 +1,9 @@
+#include "PR/os_internal.h"
+
+const char gSaCertFname[]        = "cert.sys";
+const char gSaRlFname[]          = "crl.sys";
+const char gSaAppExt[]           = "app";
+const char gSaRecExt[]           = "rec";
+const char gSaTicketFname[]      = "ticket.sys";
+const char gSaRecryptKeyFname[]  = "recrypt.sys";
+const char gSaClubTitle[]        = "iQue Club";
