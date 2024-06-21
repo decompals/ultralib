a011ebf4b8685b64de838f112fbfa95ca3584bae
Derek Hensley <hensley.derek58@gmail.com>
2024-06-21 12:19:33 -0700

    osInvalICache
    Co-authored-by: Tharo <17233964+Thar0@users.noreply.github.com>

--- src/os/invalicache.s
+++ src/os/invalicache.s
@@ -4,15 +4,25 @@
 
 .text
 LEAF(osInvalICache)
+    /* If the amount to invalidate is less than or equal to 0, return immediately */
 	blez a1, 2f
 
+    /*
+     * If the amount to invalidate is as large as or larger than
+     * the instruction cache size, invalidate all
+     */
 	li t3, ICACHE_SIZE
 	bgeu a1, t3, 3f
 
+    /*
+     * ensure end address does not wrap around and end up smaller
+     * than the start address
+    */
 	move t0, a0
 	addu t1, a0, a1
 	bgeu t0, t1, 2f
 
+    /* Mask and subtract to align to cache line */
 	addiu t1, t1, -ICACHE_LINESIZE
 	andi t2, t0, ICACHE_LINEMASK
 	subu t0, t0, t2
