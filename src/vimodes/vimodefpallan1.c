bed467a67172de611cde15744eb659f996f3d157
Derek Hensley <hensley.derek58@gmail.com>
2024-08-18 16:44:32 -0600

    LAN1 vimode comments
    Co-authored-by: Tharo <17233964+Thar0@users.noreply.github.com>

--- src/vimodes/vimodefpallan1.c
+++ src/vimodes/vimodefpallan1.c
@@ -1,3 +1,13 @@
+/**
+ * @file vimodefpallan1.c
+ *
+ * FPAL LAN1 Video Mode
+ *
+ * L = Low Resolution
+ * A = Anti-Aliased
+ * N = Deinterlaced
+ * 1 = 16-bit Framebuffer
+ */
 #include "PR/os.h"
 #include "PR/rcp.h"
 #include "PRinternal/viint.h"
--- src/vimodes/vimodempallan1.c
+++ src/vimodes/vimodempallan1.c
@@ -1,3 +1,13 @@
+/**
+ * @file vimodempallan1.c
+ *
+ * MPAL LAN1 Video Mode
+ *
+ * L = Low Resolution
+ * A = Anti-Aliased
+ * N = Deinterlaced
+ * 1 = 16-bit Framebuffer
+ */
 #include "PR/os.h"
 #include "PR/rcp.h"
 #include "PRinternal/viint.h"
